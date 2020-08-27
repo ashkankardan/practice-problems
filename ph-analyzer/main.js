9ee8c3d5ad1759c6638588a93cdce34aef0c07e3
Ashkan Kardan <ashkankardan14@gmail.com>
2020-08-27 20:49:29 +0300

    completed ph-analyzer

---
+++ ph-analyzer/main.js
@@ -0,0 +1,16 @@
+function phAnalyzer(ph){
+  if (ph >= 0 && ph < 7) {
+    return "Acidic";
+  } else if(ph === 7) {
+    return "Neutral";
+  } else if (ph < 14 && ph > 7) {
+    return "Alkaline";
+  } else {
+    return "invalid pH value";
+  }
+}
+
+console.log(phAnalyzer(7));
+console.log(phAnalyzer(4));
+console.log(phAnalyzer(12));
+console.log(phAnalyzer(25));
