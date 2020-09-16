0fe512b049dc916c0265261714b5d2793ff0e5d3
Ashkan Kardan <ashkankardan14@gmail.com>
2020-09-16 20:20:25 +0300

    completed Zip Code Validator

---
+++ zip-code-validator/main.js
@@ -0,0 +1,16 @@
+function zipCodeValidator(zipToValidate) {
+  if (zipToValidate.length === 5) {
+    for (var i = 0; i < zipToValidate.length; i++) {
+      if (typeof parseInt(zipToValidate[i]) !== "number") {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(`zipCodeValidator("21594")`, zipCodeValidator("21594"));
+console.log(`zipCodeValidator("3Eds32")`, zipCodeValidator("3Eds32"));
+console.log(`zipCodeValidator("3Eds32")`, zipCodeValidator("3Eds32"));
