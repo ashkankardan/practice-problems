362781ae3a0bf6791801c3a09dac891827c8ef34
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-11 14:51:04 +0300

    Word Counter

---
+++ word-count/main.js
@@ -0,0 +1,11 @@
+const countWords = string => string.split(" ").length
+
+console.log(
+  'countWords("Just an example here move along")',
+  countWords("Just an example here move along")
+);
+console.log('countWords("This is a test")', countWords("This is a test"));
+console.log(
+  'countWords("What an easy task, right")',
+  countWords("What an easy task, right")
+);
