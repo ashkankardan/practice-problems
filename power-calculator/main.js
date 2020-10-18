3878fecb2a2d63db423df85287e71ecbb3d49215
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-18 23:27:28 +0300

    complete Power Calculator

---
+++ power-calculator/main.js
@@ -0,0 +1,5 @@
+const circuitPower = (voltage, current) => voltage * current;
+
+console.log(`circuitPower(230, 10)`, circuitPower(230, 10));
+console.log(`circuitPower(110, 3)`, circuitPower(110, 3));
+console.log(`circuitPower(480, 20)`, circuitPower(480, 20));
