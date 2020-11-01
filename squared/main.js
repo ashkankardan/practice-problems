931e71657e53e1ed8c8e3989c9cc63f8b8003881
Ashkan Kardan <ashkankardan14@gmail.com>
2020-11-01 20:51:48 +0300

    squared

---
+++ squared/main.js
@@ -0,0 +1,12 @@
+const squared = num => num * num
+
+squared(5)
+
+squared(9)
+
+squared(100)
+
+
+console.log(`squared(5)`, squared(5));
+console.log(`squared(9)`, squared(9));
+console.log(`squared(100)`, squared(100));
