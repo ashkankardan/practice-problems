6021023e525cebce9791be296577a1ca5cc3c15a
Ashkan Kardan <ashkankardan14@gmail.com>
2020-11-08 19:53:01 +0300

    Less Than 100

---
+++ less-than-100/main.js
@@ -0,0 +1,5 @@
+const lessThan100 = (num1, num2) => (num1 + num2) < 100;
+
+console.log(`lessThan100(22, 15)`, lessThan100(22, 15))
+console.log(`lessThan100(83, 34)`, lessThan100(83, 34))
+console.log(`lessThan100(3, 77)`, lessThan100(3, 77))
