60c5fdf6e11eed5e28a7ef0fc7d0e7896863ba3a
Ashkan Kardan <ashkankardan14@gmail.com>
2020-11-22 00:05:01 +0300

    Is Same Numbers

---
+++ is-same-num/main.js
@@ -0,0 +1,5 @@
+const isSameNum = (num1, num2) => num1 === num2
+
+console.log(`isSameNum(4, 8)`, isSameNum(4, 8))
+console.log(`isSameNum(2, 2)`, isSameNum(2, 2))
+console.log(`isSameNum(2, "2")`, isSameNum(2, "2"))
